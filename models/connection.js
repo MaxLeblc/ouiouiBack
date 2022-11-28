199e55180b7eee3510920d9d449ee7c7b7ae1849
Max <maxime.leb@gmail.com>
2022-11-28 20:14:33 +0100

    node

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING
+const connectionString = "mongodb+srv://xou:sEnI1HqN5Rb8i6Ea@cluster0.mywbssa.mongodb.net/ouioui"
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
